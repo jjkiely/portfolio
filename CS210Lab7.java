f31387e7f3dbb0066cbd546027d883439b8d2701
JJ Kiely <38945339+jjkiely@users.noreply.github.com>
2018-11-23 14:58:20 +0000

    Update CS210Lab7.java

--- CS210Lab7.java
+++ CS210Lab7.java
@@ -1,53 +1,96 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 import java.util.Scanner;
-public class CS210Lab7
-{
-    public static class Stack{
+
+public class Solution {
+    public static class Stack {
         private int maxSize;
         private int[] stackArray;
         private int top;
 
-
-        public Stack(int s){
-            maxSize=s;
+        public Stack(int s) {
+            maxSize = s;
             stackArray = new int[maxSize];
-            top=-1;
+            top = -1;
         }
 
-        public void push(int j)
-        {
+        public void push(int j) {
             top++;
-            stackArray[top]=j;
+            stackArray[top] = j;
         }
 
-        public int pop()
-        {
+        public int pop() {
             return stackArray[top--];
         }
-        public int peek()
-        {
+
+        public int peek() {
             return stackArray[top];
         }
 
-        public boolean isEmpty()
-        {
-            return(top==-1);
+        public boolean isEmpty() {
+            return (top == -1);
         }
 
-        public boolean isFull()
-        {
-            return(top==maxSize-1);
+        public boolean isFull() {
+            return (top == maxSize - 1);
 
         }
-        public void makeEmpty()
-        {
-            top=-1;
+
+        public void makeEmpty() {
+            top = -1;
         }
 
     }
 
-    public static void main(String[]args)
-    {
-        Scanner sc= new Scanner(System.in);
-        Stack theStack=new Stack(100);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //int num = sc.nextInt();
+        //int num= Integer.parseInt(n);
+        String x= sc.nextLine();
+        int num=Integer.parseInt(x);
+        Stack theStack = new Stack(10);
+        String[] commands= new String[num+1];
+        for(int i=0;i<commands.length-1;i++)
+        {
+            commands[i]=sc.nextLine();
+        }
+        for(int i=0;i<commands.length-1;i++)
+        {
+            if(commands[i].substring(0,3).equals("POP"))
+            {
+                if(!theStack.isEmpty())
+                {
+                    theStack.pop();
+                }
+
+            }
+            else if(commands[i].substring(0,4).equals("PUSH"))
+            {
+                theStack.push(Integer.parseInt(commands[i].substring(5)));
+            }
+            /*else if(commands[i].substring(0,2).equals("POP"))
+            {
+                theStack.pop();
+            }
+            */
+        }
+        int max=0;
+        for(int i=0;i<commands.length-1;i++)
+        {
+            if(theStack.stackArray[i]>max)
+            {
+                max=theStack.stackArray[i];
+            }
+        }
+        if(theStack.isEmpty())
+        {
+            System.out.println("empty");
+        }
+        else{
+            System.out.println(max);
+        }
     }
 }
